59158097294f4a5cd0879a4a8a0ec490f7f29911
TahereGmi <mstgholami@gmail.com>
2023-01-21 23:57:35 +0330

    add single question page

--- pages/single-question/[id].tsx
+++ pages/single-question/[id].tsx
@@ -1,11 +1,15 @@
 import Head from 'next/head'
 import Layout from 'components/Layout/layout'
-
-const singleCardNode = <p className="text-3xl font-bold underline">
-  Hello world from single question!
-</p>
+import { IQuestionList } from '@/types/Questionlist'
+import QuestionCard from '@/components/QuestionCard/QuestionCard'
+import { selectedValue, useAppSelector } from '@/store/reducers/questionListReducer'
 
 export default function SingleQuestion() {
+  const questions = useAppSelector(selectedValue) as IQuestionList
+
+  const singleCardNode = <div className="container">
+      <QuestionCard item={questions.result[1]} />
+  </div>
   return (
     <Layout children={singleCardNode} pageTitle={'جزییات سوال'}/>
   )
